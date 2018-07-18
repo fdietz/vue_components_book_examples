1866b48f3c293a89748614769f9d8800fccec7f0
Frederik Dietz <fdietz@gmail.com>
2018-07-18 12:27:14 +0200

    add chapter 6, example 3 functional components using jsx

---
+++ chapter-6/example-3/src/components/HelloWorld.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  name: "HelloWorld",
+  functional: true,
+  render(h, { data, listeners, children }) {
+    return (
+      <div class="hello">
+        <h1>{data.attrs.msg}</h1>
+        <button onClick={listeners.click}>Click me</button>
+        {children}
+      </div>
+    );
+  }
+};
+</script>
