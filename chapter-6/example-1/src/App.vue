d992abdce9f6e32b43aaca5bf8759bad002e2ff3
Frederik Dietz <fdietz@gmail.com>
2018-07-18 12:06:22 +0200

    chapter-6, example 1 and 2

---
+++ chapter-6/example-1/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App" @click="clickCount+=1">
+      <p>I was clicked: {{clickCount}}</p>
+    </HelloWorld>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "app",
+  data() {
+    return {
+      clickCount: 0
+    };
+  },
+  components: {
+    HelloWorld
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ chapter-6/example-1/src/components/HelloWorld.vue
@@ -0,0 +1,7 @@
+<template functional>
+  <div class="hello">
+    <h1>{{ props.msg }}</h1>
+    <button @click="listeners.click">Click me</button>
+    <slot></slot>
+  </div>
+</template>
